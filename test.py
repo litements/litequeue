270997ee62fb52a010d1495d61a5854a53be2619
polyrand <37962604+polyrand@users.noreply.github.com>
2024-03-11 22:44:16 +0100

    remove test

--- test.py
+++ test.py
@@ -24,24 +24,6 @@ def single_queue(request) -> LiteQueue:
     return _q
 
 
-@pytest.fixture(scope="function", params=["_pop_transaction", "_pop_returning"])
-def multi_queue(request) -> Tuple[LiteQueue, LiteQueue]:
-    """
-    Create a tmp dir, create a DB in it with multiple queues.
-    """
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        db_path = Path(tmpdir) / "test-queue.sqlite3"
-        q1 = LiteQueue(db_path, queue_name="queue1")
-        q2 = LiteQueue(db_path, queue_name="queue2")
-
-        for _q in [q1, q2]:
-            if _q.get_sqlite_version() > 35:
-                _q.pop = getattr(_q, request.param[1])
-
-        return q1, q2
-
-
 @pytest.fixture(scope="function")
 def queue_with_data(single_queue) -> LiteQueue:
     q = single_queue
